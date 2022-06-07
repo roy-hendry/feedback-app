8ea8c73b1d5c5d8b7f0aac556f7b81091656c9c1
roy-hendry <royjairusraj@hotmail.com>
2022-06-07 08:48:13 +0100

    Managing global state

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,17 @@
+import FeedbackItem from "./FeedbackItem"
+
+function FeedbackList({feedback}) {
+    console.log(feedback)
+    if(!feedback || feedback.length === 0) {
+        return <p>No Feedback yet </p>
+    }
+  return (
+    <div className="feedback-list">
+        {feedback.map((item) => (
+            <FeedbackItem key={item.id} item={item} />
+        ))}
+    </div>
+  )
+}
+
+export default FeedbackList
