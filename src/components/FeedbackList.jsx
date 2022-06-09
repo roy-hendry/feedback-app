775d60f09476be6c29bd5c4bade641caa3516941
roy-hendry <royjairusraj@hotmail.com>
2022-06-09 08:51:51 +0100

    Events and prop drilling

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,15 +2,17 @@ import PropTypes from "prop-types"
 
 import FeedbackItem from "./FeedbackItem"
 
-function FeedbackList({feedback}) {
-    console.log(feedback)
+function FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0) {
         return <p>No Feedback yet </p>
     }
   return (
     <div className="feedback-list">
         {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item} />
+            <FeedbackItem
+            key={item.id}
+            item={item}
+            handleDelete={handleDelete}/>
         ))}
     </div>
   )
